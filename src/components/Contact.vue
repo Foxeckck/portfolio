653c5885289c05d8fc41915201d9aa5d0b5906c5
foxeckck <free999game999@gmail.com>
2024-02-04 19:27:41 +0100

    тг бот

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -10,14 +10,55 @@
                 <li><a href="">vufka33@gmail.com</a></li>
                 <li><a href="">+49151520495185</a></li>
             </ul>
-            <div class="form">
+            <form class="form" @submit.prevent="sendMessage">
                 <h3>Обращяйтесь ко мне</h3>
-                <textarea></textarea>
-                <button>Отправить</button>
-            </div>
+                <textarea v-model="message" id="message"></textarea>
+                <button type="submit">Отправить</button>
+            </form>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "id",
+        };
+    },
+    methods: {
+        sendMessage() {
+            const chatId = "5084794202"; // Замените на свой chat_id
+            const url = "https://api.telegram.org/bot6449439101:AAHgmB9NycU_eziy4NpuI0wsMmNH6uUyvnM/sendMessage"
+
+            if(this.message){
+// Формируем тело запроса
+const formData = new FormData();
+            formData.append("chat_id", chatId);
+            formData.append("text", this.message);
+
+            // Отправляем POST запрос к API Telegram Bot
+            fetch(url, {
+                method: "POST",
+                body: formData,
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log("Ответ от Telegram API:", data);
+                    // Обработка ответа, если необходимо
+                })
+                .catch(error => {
+                    console.error("Ошибка при отправке сообщения:", error);
+                    // Обработка ошибки, если необходимо
+                });
+                this.message = ""
+            }
+
+                
+        },
+    },
+};
+</script>
 
 <style scoped>
 .skills {
@@ -56,7 +97,8 @@
     justify-content: space-around;
     flex-wrap: wrap;
 }
-.item{
+
+.item {
     min-width: 150px;
     display: flex;
     text-align: center;
@@ -65,11 +107,13 @@
     flex-direction: column;
     margin: 20px 0 10px 0;
 }
-.grid{
+
+.grid {
     display: grid;
     grid-template-columns: 1fr 2fr;
 }
-.form{
+
+.form {
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -77,7 +121,7 @@
     border-left: 2px solid black;
 }
 
-.form textarea{
+.form textarea {
     width: 80%;
     height: 120px;
     background-color: rgb(255, 255, 255);
@@ -86,7 +130,7 @@
     border-radius: 10px;
 }
 
-.form button{
+.form button {
     border: 2px solid black;
     color: black;
     padding: 5px 10px;
@@ -96,7 +140,7 @@
     margin-top: 10px;
 }
 
-.info{
+.info {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -108,7 +152,7 @@
 
 }
 
-.info li{
+.info li {
     list-style: none;
     text-align: center;
     border: 2px solid black;
@@ -117,9 +161,7 @@
     width: 100%;
     box-sizing: border-box;
 
-    
-}
 
+}
 </style>
 
-<script></script>
